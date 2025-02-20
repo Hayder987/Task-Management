9cba5b869a96cb942da49c6a94c8d77901858d88
Hayder Ali <hayderbd4290@gmail.com>
2025-02-21 01:54:51 +0600

    fix some issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,50 @@
-import { LuLogOut } from "react-icons/lu"
-import useAuth from "../hook/useAuth"
-import { useNavigate } from "react-router"
-
+import { LuLogOut } from "react-icons/lu";
+import useAuth from "../hook/useAuth";
+import { useNavigate } from "react-router";
 
 const Navbar = () => {
-  const {user, logoutUser} = useAuth()
-  const navigate = useNavigate()
+  const { user, logoutUser } = useAuth();
+  const navigate = useNavigate();
+
+  const logOutHandler = async () => {
+    await logoutUser();
+     navigate("/");
+  };
 
   return (
     <div className=" flex justify-between items-center px-6 py-4">
-       <div className="">
-        <h1 className="font-bold text-4xl">Task <span className="text-blue-800">Flow</span></h1>
-       </div>
-       {/* user */}
-       <div className="flex items-center gap-3 border p-1 border-gray-200 rounded-md">
-          <div className="">
-            {
-              user?.photoURL?
-              <img referrerPolicy="no-referrer" src={`${user?.photoURL}`} alt="" className="w-12 h-12 rounded-full" />:
-              <div className="w-12 h-12 font-semibold bg-blue-200 rounded-full p-4 flex justify-center items-center">
-                <h1 className="text-blue-800 text-2xl">{user?.displayName.slice(0,2).toUpperCase()}</h1>
-              </div>
-            }
-          </div>
-          <button
-          onClick={async()=>{
-           await logoutUser()
-           navigate('/')
-          }}
-           className="text-4xl text-blue-800 cursor-pointer"><LuLogOut /></button>
-       </div>
+      <div className="">
+        <h1 className="font-bold text-4xl">
+          Task <span className="text-blue-800">Flow</span>
+        </h1>
+      </div>
+      {/* user */}
+      <div className="flex items-center gap-3 border p-1 border-gray-200 rounded-md">
+        <div className="">
+          {user?.photoURL ? (
+            <img
+              referrerPolicy="no-referrer"
+              src={`${user?.photoURL}`}
+              alt=""
+              className="w-12 h-12 rounded-full"
+            />
+          ) : (
+            <div className="w-12 h-12 font-semibold bg-blue-200 rounded-full p-4 flex justify-center items-center">
+              <h1 className="text-blue-800 text-2xl">
+                {user?.displayName.slice(0, 2).toUpperCase()}
+              </h1>
+            </div>
+          )}
+        </div>
+        <button
+          onClick={logOutHandler}
+          className="text-4xl text-blue-800 cursor-pointer"
+        >
+          <LuLogOut />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -3,10 +3,11 @@ import GoogleLogin from "../components/GoogleLogin";
 import banner from "../assets/images/login2.jpg";
 import Swal from "sweetalert2";
 import useAuth from "../hook/useAuth";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 const Login = () => {
-  const {loginUser} = useAuth();
-  const navigate = useNavigate()
+  const { loginUser } = useAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,7 +26,7 @@ const Login = () => {
         timer: 1500,
       });
       form.reset();
-      navigate('/task')
+      navigate("/task");
     } catch (err) {
       Swal.fire(`${err?.message}`);
     }
@@ -33,59 +34,67 @@ const Login = () => {
 
   return (
     <div className="bg-slate-100 flex justify-center items-center min-h-[100vh]">
-      <div className="container p-4 rounded-lg bg-white flex items-center mx-auto">
-        {/* image */}
-        <div className="hidden px-20 md:flex md:w-1/2">
-          <img src={banner} alt="" className="w-full h-full" />
-        </div>
-        {/* form */}
-        <div className="w-full md:w-1/2 py-10 lg:px-32">
-          <h1 className="text-center text-3xl font-semibold mb-8">Sign In</h1>
-          <form
-          onSubmit={handleSubmit}
-           className="">
-            <div className="px-4 flex flex-col gap-5 ">
-              {/* email */}
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  className="input input-bordered w-full"
-                  required
-                />
-              </div>
-              {/* password */}
-              <div className="form-control mb-4">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  className="input input-bordered w-full"
-                  required
-                />
-              </div>
-              <input
-                type="submit"
-                value="Login Now"
-                className="py-2 rounded-md font-semibold cursor-pointer px-6 bg-blue-800 text-white"
-              />
-            </div>
-          </form>
-          <div className="divider my-6">OR</div>
-          <GoogleLogin></GoogleLogin>
-          <p className="text-center py-4 font-medium">
-            Don't Have Account?{" "}
-            <Link to="/register">
-              <span className="text-blue-600 underline"> Register Now</span>
-            </Link>
+      <div className="container p-4 rounded-lg bg-white mx-auto">
+        <Link to='/'>
+          <p className="flex items-center gap-2 px-4 cursor-pointer py-2">
+            <span className="">
+              <FaArrowLeftLong />
+            </span>
+            <span className="">Back to Home</span>
           </p>
+        </Link>
+        <div className="flex items-center">
+          {/* image */}
+          <div className="hidden px-20 md:flex md:w-1/2">
+            <img src={banner} alt="" className="w-full h-full" />
+          </div>
+          {/* form */}
+          <div className="w-full md:w-1/2 py-10 lg:px-32">
+            <h1 className="text-center text-3xl font-semibold mb-8">Sign In</h1>
+            <form onSubmit={handleSubmit} className="">
+              <div className="px-4 flex flex-col gap-5 ">
+                {/* email */}
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div>
+                {/* password */}
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="input input-bordered w-full"
+                    required
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Login Now"
+                  className="py-2 rounded-md font-semibold cursor-pointer px-6 bg-blue-800 text-white"
+                />
+              </div>
+            </form>
+            <div className="divider my-6">OR</div>
+            <GoogleLogin></GoogleLogin>
+            <p className="text-center py-4 font-medium">
+              Don't Have Account?{" "}
+              <Link to="/register">
+                <span className="text-blue-600 underline"> Register Now</span>
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
